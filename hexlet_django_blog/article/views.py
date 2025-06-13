d3f24fd2b851188d890282d1805ea8d5e057afaf
SergeyAnuf <sergeysyava38@gmail.com>
2025-06-13 20:05:27 +0300

    выполнен шаблон для приложения /article

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("article")
+    context = {'app_name': 'article'}
+    return render(request, 'articles/index.html', context)
