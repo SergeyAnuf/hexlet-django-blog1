e1e66ef2f616f8a539ea5b972312e80e319caf21
SergeyAnuf <sergeysyava38@gmail.com>
2025-06-14 19:21:39 +0300

    добавлено удаление CRUD

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -6,6 +6,7 @@ from hexlet_django_blog.article.views import (
     ArticleCommentsView, 
     ArticleFormCreateView,
     ArticleFormEditView,
+    ArticleFormDeleteView,
 )
 
 
@@ -14,5 +15,6 @@ urlpatterns = [
     path("<int:id>/edit/", ArticleFormEditView.as_view(), name="articles_update"),
     path("<int:id>/", ArticleView.as_view()),
     path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
-    path("create/", ArticleFormCreateView.as_view(), name="articles_create"),   
+    path("create/", ArticleFormCreateView.as_view(), name="articles_create"),
+    path("<int:id>/delete/", ArticleFormDeleteView.as_view(), name="articles_delete"),   
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -75,3 +75,12 @@ class ArticleFormEditView(View):
         return render(
             request, "articles/update.html", {"form": form, "article_id": article_id}
         )
+    
+
+class ArticleFormDeleteView(View):
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = Article.objects.get(id=article_id)
+        if article:
+            article.delete()
+        return redirect("/articles")
