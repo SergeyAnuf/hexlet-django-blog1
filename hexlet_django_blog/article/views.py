96bf02dce50797b75b1ebc0ab15299797222f337
SergeyAnuf <sergeysyava38@gmail.com>
2025-06-14 12:47:24 +0300

    добавлен просмотр CRUD , изменены urls.py, views.py добавлен show.ntml

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.views import View
@@ -16,3 +16,24 @@ class IndexView(View):
                 "articles": articles,
             },
         )
+    
+
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs["id"])
+        return render(
+            request,
+            "articles/show.html",
+            context={
+                "article": article,
+            },
+        )
+    
+
+class ArticleCommentsView(View):
+    def get(self, request, *args, **kwargs):
+        comment = get_object_or_404(
+            Comment, id=kwargs["id"], article__id=kwargs["article_id"]
+        )
+
+        return render(...)
