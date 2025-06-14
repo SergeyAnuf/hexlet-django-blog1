a2ea296b76864809b7ab3642809447dec42988a3
SergeyAnuf <sergeysyava38@gmail.com>
2025-06-14 15:43:22 +0300

    добавлено обновление CRUD

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleCommentsView, ArticleFormCreateView
+from hexlet_django_blog.article.views import (
+    IndexView, 
+    ArticleView, 
+    ArticleCommentsView, 
+    ArticleFormCreateView,
+    ArticleFormEditView,
+)
+
 
 urlpatterns = [
     path("", IndexView.as_view()),
+    path("<int:id>/edit/", ArticleFormEditView.as_view(), name="articles_update"),
     path("<int:id>/", ArticleView.as_view()),
     path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
-    path("create/", ArticleFormCreateView.as_view(), name="articles_create"),
+    path("create/", ArticleFormCreateView.as_view(), name="articles_create"),   
 ]
